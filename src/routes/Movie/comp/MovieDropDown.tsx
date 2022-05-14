116ea4391940b2f2d1de6a78875bd91d75a51d9e
lumpenop <swk5373@gmail.com>
2022-05-15 01:17:05 +0900

    Feat: dropdown/즐겨찾기 sorting  구현

--- src/routes/Movie/comp/MovieDropDown.tsx
+++ src/routes/Movie/comp/MovieDropDown.tsx
@@ -1,45 +1,69 @@
-import { useState } from 'react'
+import { SetStateAction, useEffect, useState } from 'react'
+import { useMount } from 'react-use'
 
 import styles from '../movie.module.scss'
-import { cx } from 'styles'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
-
-const dropMenu = ['연도별 오름차순', '연도별 내림차순', '이름 오름차순', '이름 내림차순']
+import { useRecoil } from 'hooks/state'
+import { favoriteData } from 'states/movieStates'
+import { IMovie } from 'types/movie'
 
 const MovieDropDown = () => {
-  const [selectedValue, setSelectedValue] = useState<string>('연도별 내림차순, 이름 내림차순')
-  const [isMenuOpen, setIsMunuOpen] = useState<boolean>(false)
+  const [favoData, setFavoData] = useRecoil(favoriteData)
+  const [year, setYear] = useState<string>('내림차순')
+  const [title, setTitle] = useState<string>('오름차순')
 
-  const handleClickMenu = (value: string) => {
-    const prevValue = selectedValue.split(', ').filter((element) => {
-      return !element.includes(value.split(' ')[0])
-    })
-    setSelectedValue(() => `${prevValue}, ${value}`)
+  useMount(() => {
+    favoSort()
+  })
+
+  useEffect(() => {
+    favoSort()
+  }, [year, title])
+
+  const handleYearSelect = (event: { currentTarget: { value: SetStateAction<string> } }) => {
+    setYear(event.currentTarget.value)
   }
 
-  const handleClickMenuOpen = () => {
-    setIsMunuOpen((prev) => !prev)
+  const handleTitleSelect = (event: { currentTarget: { value: SetStateAction<string> } }) => {
+    setTitle(event.currentTarget.value)
+  }
+
+  const favoSort = () => {
+    const sortedFavo = [...favoData].sort((a: IMovie, b: IMovie): number => {
+      if (year === '내림차순') {
+        if (a.Year > b.Year) return -1
+        if (a.Year < b.Year) return 1
+      }
+      if (year === '오름차순') {
+        if (a.Year > b.Year) return 1
+        if (a.Year < b.Year) return -1
+      }
+      if (title === '내림차순') {
+        if (a.Title > b.Title) return -1
+        if (a.Title < b.Title) return 1
+      }
+      if (title === '오름차순') {
+        if (a.Title > b.Title) return 1
+        if (a.Title < b.Title) return -1
+      }
+      return 0
+    })
+
+    setFavoData(sortedFavo)
   }
 
   return (
     <div className={styles.dropContainer}>
-      <button type='button' onClick={handleClickMenuOpen}>
-        <input type='text' value={selectedValue} readOnly />
-        <FontAwesomeIcon className={styles.dropIcon} icon={faCaretDown} />
-      </button>
-      <ul className={cx(styles.dropMenuList, { [styles.menuOn]: isMenuOpen })}>
-        {dropMenu.map((element, index) => {
-          return (
-            <li className={styles.dropMenu} key={`${element}${index}`}>
-              <div>
-                <input type='button' value={element} onClick={(e) => handleClickMenu(e.currentTarget.value)} />
-              </div>
-            </li>
-          )
-        })}
-      </ul>
+      연도별
+      <select name='연도별' defaultValue='내림차순' onChange={handleYearSelect}>
+        <option value='내림차순'>내림차순</option>
+        <option value='오름차순'>오름차순</option>
+      </select>
+      제목별
+      <select name='제목별' defaultValue='오름차순' onChange={handleTitleSelect}>
+        <option value='내림차순'>내림차순</option>
+        <option value='오름차순'>오름차순</option>
+      </select>
     </div>
   )
 }
